2b441c5f379b87ca4f359d54f81b4dd1f000f5b0
Alexis Bourgois <alexis.bourgois@celsius-online.com>
2024-02-10 13:42:10 +0100

    Initialize

---
+++ CustomDatas/PageInfos.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.CustomDatas {
+    public struct PageInfos {
+        public string id;
+        public string name;
+        public string buttonId;
+
+        public PageInfos(string id, string name, string buttonId) {
+            this.id = id;
+            this.name = name;
+            this.buttonId = buttonId;
+        }
+    }
+}
