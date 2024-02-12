7c315c62d133e6bd5170549214c3758ef9bf0792
Alexis Bourgois <alexis.bourgois@celsisus-online.com>
2024-02-12 14:37:09 +0100

    A bit of styling

---
+++ CustomDatas/ApplicationConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp1.CustomDatas {
+    public static class ApplicationConstants {
+        public static string[] palette = new string[] {
+            "#22223b",
+            "#4a4e69",
+            "#9a8c98",
+            "#c9ada7",
+            "#f2e9e4"
+        };
+
+        public static string neutralLight = "#eae0d5";
+        public static string neutralDark = "#0a0908";
+    }
+}
