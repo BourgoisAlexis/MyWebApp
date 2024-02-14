ad7b8df72ef4356857d8e72809cc087edc5044fd
Alexis Bourgois <alexis.bourgois@celsisus-online.com>
2024-02-14 09:36:27 +0100

    Calculator creation

--- obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575ba7449f776b41fc892bf9c72455e5a5a66d19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c315c62d133e6bd5170549214c3758ef9bf0792")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
