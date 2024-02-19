33aafd629f541ec2d4f6d91b7c68f4ccd9a5ce23
Alexis Bourgois <alexis.bourgois@celsius-online.com>
2024-02-19 13:39:55 +0100

    hover with background image

--- obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7b8df72ef4356857d8e72809cc087edc5044fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6c23a1e3f886b92b5ff985096ad18ae165450d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
