1e0241aec1da61480388bc8f80290733b1829173
Alexis Bourgois <alexis.bourgois@celsius-online.com>
2024-02-20 13:38:32 +0100

    Reworking hovers

---
+++ CustomDatas/ElementInfos.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.CustomDatas {
+    public struct ElementInfos {
+        public string title;
+        public string content;
+        public string background;
+
+        public ElementInfos(string title, string content, string background) { 
+            this.title = title;
+            this.content = content;
+            this.background = background;
+        }
+    }
+}
--- obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6c23a1e3f886b92b5ff985096ad18ae165450d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33aafd629f541ec2d4f6d91b7c68f4ccd9a5ce23")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
